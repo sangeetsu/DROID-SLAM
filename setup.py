efa520b9ca5fb6ed694a0e43a9ed8f7e308e6843
sangeetsu <sangeetsu@gmail.com>
2023-04-14 08:16:12 -0700

    444 specific env files

--- setup.py
+++ setup.py
@@ -22,8 +22,6 @@ setup(
                     '-gencode=arch=compute_61,code=sm_61',
                     '-gencode=arch=compute_70,code=sm_70',
                     '-gencode=arch=compute_75,code=sm_75',
-                    '-gencode=arch=compute_80,code=sm_80',
-                    '-gencode=arch=compute_86,code=sm_86',
                 ]
             }),
     ],
@@ -51,9 +49,7 @@ setup(
                     '-gencode=arch=compute_60,code=sm_60', 
                     '-gencode=arch=compute_61,code=sm_61', 
                     '-gencode=arch=compute_70,code=sm_70', 
-                    '-gencode=arch=compute_75,code=sm_75',
-                    '-gencode=arch=compute_80,code=sm_80',
-                    '-gencode=arch=compute_86,code=sm_86',                 
+                    '-gencode=arch=compute_75,code=sm_75',                                    
                 ]
             }),
     ],
